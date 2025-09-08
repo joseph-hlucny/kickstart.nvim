13e4b7f396a5d02616ea8c1e56293a983f6c9c41
Joseph Hlucny <josephhlucny@outlook.com>
2025-09-07 21:10:54 -0500

    Disable mouse... who needs that thing anyways.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,6 @@
+-- Load custom configuration
+require 'custom.config'
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
